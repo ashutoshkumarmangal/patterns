b201f828a4de7d20971384bfcd4f6b372ea495ab
ashutoshkumarmangal <aashuakm12@gmail.com>
2023-03-24 22:45:59 +0530

    patterns

---
+++ alphabetPatterns/D.java
@@ -0,0 +1,18 @@
+public class D {
+    //public class C {
+        public static void main(String[] args) {
+            int n = 20;
+            for(int i = 0; i< n;i++){
+                for (int j = 0; j<n ;j++){
+                    if(){
+                    System.out.print("*");
+                    }
+                    else 
+                    System.out.print(" ");
+                }
+                System.out.println();
+            }
+        }
+    }
+    
+
---
+++ otherPatterns/triangle1.java
@@ -0,0 +1,20 @@
+public class triangle1 {
+    public static void main(String[] args) {
+        
+//public class barfi {
+   
+    //public static void main(String[] args) {
+        int n = 21;
+        for(int i = 0; i< n;i++){
+            for (int j = 0; j<n ;j++){
+                if( i+j<=(n-1)/2 || i==0 && j<(n-1)/2 || j==0 && i<(n-1)/2 ){
+                System.out.print("*");
+                }
+                else 
+                System.out.print(" ");
+            }
+            System.out.println();
+        }
+    }
+    }
+
